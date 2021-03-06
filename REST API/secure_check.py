88849de7d844859ab181261f5b4f094eebd8f436
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2021-03-05 22:06:32 -0700

    Update secure_check.py

--- REST API/secure_check.py
+++ REST API/secure_check.py
@@ -9,6 +9,18 @@ userid_table = {u.id: u for u in users}
 
 def authenticate(username, password):
 
+    user = username_table(username,None)
+
+    if user and password == user.password:
+        return user
+
+def identity(payload):
+
+    user_id = payload['identity']
+    return userid_table.get(user_id,None)
+
+
+
 
 
 
