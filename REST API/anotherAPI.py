b666e9fd9495bd69161ac0c755097afc0c9b6c8d
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2021-03-06 22:26:26 -0700

    Update anotherAPI.py

--- REST API/anotherAPI.py
+++ REST API/anotherAPI.py
@@ -2,30 +2,55 @@ from flask import Flask
 from flask_restful import Resource, Api
 from secure_check import authenticate, identity
 from flask_jwt import JWT, jwt_required
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecretkey'
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
+app.config['SQLACHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+Migrate(app,db)
 
 api = Api(app)
 jwt = JWT(app, authenticate, identity)
 
-players = []
+####################################
+
+class Player(db.Model):
+
+    name = db.Column(db.String(100), primary_key=True)
+
+    def __init__(self, name):
+
+        self.name = name
+
+    def json(self):
+
+        return {'name':self.name}
+
+####################################
 
 class PlayerNames(Resource):
 
     def get(self, name):
 
-        for player in players:
-            if player['name'] == name: 
-                return player 
-        
-        return {'name': None}, 404
+        player = Player.query.filter_by(name=name).first()
+
+        if player:
+            return player.json()
+        else:
+            return {'name': None}, 404
 
     def post(self, name):
         
-        player = {'name':name}
-        players.append(player)
-        return player
+        player = Player(name=name)
+        db.session.add(player)
+        db.session.commit()
+
+        return player.json()
 
     def delete(self, name):
         
@@ -48,6 +73,4 @@ api.add_resource(AllNames, '/players')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
 
